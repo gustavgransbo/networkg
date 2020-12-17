e61e421563262f0f1820ef608edcb549abdd74a6
Gustav Gränsbo <gustav.gransbo@gmail.com>
2020-12-17 20:59:34 +0100

    Set up Python development tooling

---
+++ noxfile.py
@@ -0,0 +1,20 @@
+import nox
+from nox.sessions import Session
+
+nox.options.reuse_existing_virtualenvs = True
+
+
+@nox.session(python="3.8")
+def lint(session: Session):
+    """ Lint Python code using flake8 """
+    args = session.posargs or ["networkg"]
+    session.install(
+        "black",
+        "flake8",
+        "isort",
+        "isort",
+        "black",
+        "-c",
+        "dev-requirements.txt",
+    )
+    session.run("flake8", *args)
