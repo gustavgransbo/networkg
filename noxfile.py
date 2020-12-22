e460507fa9097d954ffc6d4f6b58b2ecb023168d
Gustav Gränsbo <gustav.gransbo@gmail.com>
2020-12-22 19:51:54 +0100

    Add mypy nox-session

--- noxfile.py
+++ noxfile.py
@@ -23,6 +23,15 @@ def lint(session: Session):
     session.run("flake8", *args)
 
 
+@nox.session(python="3.8")
+def mypy(session: Session) -> None:
+    """Type-check using mypy."""
+    args = session.posargs or ["networkg"]
+    session.install("mypy", "maturin", "-c", "requirements-dev.txt")
+    session.run("maturin", "develop")
+    session.run("mypy", *args)
+
+
 @nox.session(python=["3.7", "3.8", "3.9"])
 def xdoctest(session: Session) -> None:
     """Run Python examples with xdoctest."""
